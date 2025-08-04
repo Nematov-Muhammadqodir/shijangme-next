0536628b8b265984885993af672000dbf1dab116
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-08-04 14:54:20 +0900

    feat: develop MemberFollowings sectional component

--- libs/components/member/MemberFollowers.tsx
+++ libs/components/member/MemberFollowers.tsx
@@ -1,7 +1,188 @@
-import React from "react";
+import { Follower } from "@/libs/types/follow/follow";
+import { FollowInquiry } from "@/libs/types/follow/follow.input";
+import { Box, Button, Pagination, Stack, Typography } from "@mui/material";
+import { useRouter } from "next/router";
+import React, { ChangeEvent, useState } from "react";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
-const MemberFollowers = () => {
-  return <div>MemberFollowers</div>;
+interface MemberFollowsProps {
+  initialInput: FollowInquiry;
+  subscribeHandler: any;
+  unsubscribeHandler: any;
+  redirectToMemberPageHandler: any;
+  likeMemberHandler: any;
+}
+
+const MemberFollowers = (props: MemberFollowsProps) => {
+  const {
+    initialInput,
+    subscribeHandler,
+    unsubscribeHandler,
+    redirectToMemberPageHandler,
+    likeMemberHandler,
+  } = props;
+  const [total, setTotal] = useState<number>(0);
+  const router = useRouter();
+  const category: any = router.query?.category ?? "products";
+  const [followInquiry, setFollowInquiry] =
+    useState<FollowInquiry>(initialInput);
+  const [memberFollowers, setMemberFollowers] = useState<Follower[]>([]);
+  // const memberFollowers = [1, 2, 3, 4];
+
+  /** HANDLERS **/
+  const paginationHandler = async (
+    event: ChangeEvent<unknown>,
+    value: number
+  ) => {
+    followInquiry.page = value;
+    setFollowInquiry({ ...followInquiry });
+  };
+  return (
+    <div id="member-follows-page">
+      <Stack className="main-title-box">
+        <Stack className="right-box">
+          <Typography className="main-title">
+            {category === "followers" ? "Followers" : "Followings"}
+          </Typography>
+        </Stack>
+      </Stack>
+      <Stack className="follows-list-box">
+        <Stack className="listing-title-box">
+          <Typography className="title-text">Name</Typography>
+          <Typography className="title-text">Details</Typography>
+          <Typography className="title-text">Subscription</Typography>
+        </Stack>
+        {memberFollowers?.length === 0 && (
+          <div className={"no-data"}>
+            <img src="/img/icons/icoAlert.svg" alt="" />
+            <p>No Followers yet!</p>
+          </div>
+        )}
+        {memberFollowers.map((follower: Follower) => {
+          return (
+            <Stack className="follows-card-box" key={follower._id}>
+              <Stack
+                className={"info"}
+                onClick={() =>
+                  redirectToMemberPageHandler(follower?.followerData?._id)
+                }
+              >
+                <Stack className="image-box">
+                  <img src={"/img/profile/defaultImg.jpg"} alt="" />
+                </Stack>
+                <Stack className="information-box">
+                  <Typography className="name">
+                    {follower?.followerData?.memberNick}
+                  </Typography>
+                </Stack>
+              </Stack>
+              <Stack className={"details-box"}>
+                <Box className={"info-box"} component={"div"}>
+                  <p>Followers</p>
+                  <span>({follower?.followerData?.memberFollowers})</span>
+                </Box>
+                <Box className={"info-box"} component={"div"}>
+                  <p>Followings</p>
+                  <span>({follower?.followerData?.memberFollowings})</span>
+                </Box>
+                <Box className={"info-box"} component={"div"}>
+                  {follower?.meLiked && follower?.meLiked[0]?.myFavorite ? (
+                    <FavoriteIcon
+                      color="primary"
+                      onClick={() =>
+                        likeMemberHandler(
+                          follower?.followerData?._id,
+                          followInquiry
+                        )
+                      }
+                    />
+                  ) : (
+                    <FavoriteBorderIcon
+                      onClick={() =>
+                        likeMemberHandler(
+                          follower?.followerData?._id,
+                          followInquiry
+                        )
+                      }
+                    />
+                  )}
+                  <span>({follower?.followerData?.memberLikes})</span>
+                </Box>
+              </Stack>
+              {true && (
+                <Stack className="action-box">
+                  {follower.meFollowed &&
+                  follower.meFollowed[0]?.myFollowing ? (
+                    <>
+                      <Typography>Following</Typography>
+                      <Button
+                        variant="outlined"
+                        sx={{
+                          background: "#ed5858",
+                          ":hover": { background: "#ee7171" },
+                        }}
+                        onClick={() =>
+                          unsubscribeHandler(
+                            follower?.followerData?._id,
+                            followInquiry
+                          )
+                        }
+                      >
+                        Unfollow
+                      </Button>
+                    </>
+                  ) : (
+                    <Button
+                      variant="contained"
+                      sx={{
+                        background: "#60eb60d4",
+                        ":hover": { background: "#60eb60d4" },
+                      }}
+                      onClick={() =>
+                        subscribeHandler(
+                          follower?.followerData?._id,
+                          followInquiry
+                        )
+                      }
+                    >
+                      Follow
+                    </Button>
+                  )}
+                </Stack>
+              )}
+            </Stack>
+          );
+        })}
+      </Stack>
+      {memberFollowers.length !== 0 && (
+        <Stack className="pagination-config">
+          <Stack className="pagination-box">
+            <Pagination
+              page={followInquiry.page}
+              count={Math.ceil(total / followInquiry.limit)}
+              onChange={paginationHandler}
+              shape="circular"
+              color="primary"
+            />
+          </Stack>
+          <Stack className="total-result">
+            <Typography>{total} followers</Typography>
+          </Stack>
+        </Stack>
+      )}
+    </div>
+  );
+};
+
+MemberFollowers.defaultProps = {
+  initialInput: {
+    page: 1,
+    limit: 5,
+    search: {
+      followingId: "",
+    },
+  },
 };
 
 export default MemberFollowers;
