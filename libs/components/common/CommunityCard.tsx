5aeedacb6eed81e1a57a4b54f97d936498bb818f
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-08-04 17:30:15 +0900

    feat: develop Articles sectional component

---
+++ libs/components/common/CommunityCard.tsx
@@ -0,0 +1,109 @@
+import React from "react";
+import { useRouter } from "next/router";
+import { Stack, Typography } from "@mui/material";
+import { BoardArticle } from "../../types/board-article/board-article";
+import Moment from "react-moment";
+import { useReactiveVar } from "@apollo/client";
+import IconButton from "@mui/material/IconButton";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+
+interface CommunityCardProps {
+  boardArticle: BoardArticle;
+  size?: string;
+  likeArticleHandler: any;
+}
+
+const CommunityCard = (props: CommunityCardProps) => {
+  const { boardArticle, size = "normal", likeArticleHandler } = props;
+
+  const router = useRouter();
+
+  const imagePath: string = "/img/homePage/article-default.jpg";
+
+  /** HANDLERS **/
+  const chooseArticleHandler = (
+    e: React.SyntheticEvent,
+    boardArticle: BoardArticle
+  ) => {
+    router.push(
+      {
+        pathname: "/community/detail",
+        query: {
+          articleCategory: boardArticle?.articleCategory,
+          id: boardArticle?._id,
+        },
+      },
+      undefined,
+      { shallow: true }
+    );
+  };
+
+  const goMemberPage = (id: string) => {
+    // if (id === user?._id) router.push("/mypage");
+    // else router.push(`/member?memberId=${id}`);
+    router.push(`/member?memberId=${id}`);
+  };
+
+  return (
+    <Stack
+      sx={{ width: size === "small" ? "285px" : "317px" }}
+      className="community-general-card-config"
+      onClick={(e: any) => chooseArticleHandler(e, boardArticle)}
+    >
+      <Stack className="image-box">
+        <img src={imagePath} alt="" className="card-img" />
+      </Stack>
+      <Stack className="desc-box" sx={{ marginTop: "-20px" }}>
+        <Stack>
+          <Typography
+            className="desc"
+            onClick={(e: any) => {
+              e.stopPropagation();
+              goMemberPage(boardArticle?.memberData?._id as string);
+            }}
+          >
+            {boardArticle?.memberData?.memberNick}
+          </Typography>
+          <Typography className="title">
+            {boardArticle?.articleTitle}
+          </Typography>
+        </Stack>
+        <Stack className={"buttons"}>
+          <IconButton color={"default"}>
+            <RemoveRedEyeIcon />
+          </IconButton>
+          <Typography className="view-cnt">
+            {boardArticle?.articleViews}
+          </Typography>
+          <IconButton
+            color={"default"}
+            onClick={(e: any) =>
+              likeArticleHandler(e, "user", boardArticle?._id)
+            }
+          >
+            {boardArticle?.meLiked && boardArticle?.meLiked[0]?.myFavorite ? (
+              <FavoriteIcon color={"primary"} />
+            ) : (
+              <FavoriteBorderIcon />
+            )}
+          </IconButton>
+          <Typography className="view-cnt">
+            {boardArticle?.articleLikes}
+          </Typography>
+        </Stack>
+      </Stack>
+      <Stack className="date-box">
+        <Moment className="month" format={"MMMM"}>
+          {boardArticle?.createdAt}
+        </Moment>
+        <Typography className="day">
+          <Moment format={"DD"}>{boardArticle?.createdAt}</Moment>
+        </Typography>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default CommunityCard;
