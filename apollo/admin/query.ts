bbc71c21bb32e696258a819d2b07189d37c95940
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-08-13 18:28:55 +0900

    feat: develop mutation and queries for admin

---
+++ apollo/admin/query.ts
@@ -0,0 +1,234 @@
+import { gql } from "@apollo/client";
+
+/**************************
+ *         MEMBER         *
+ *************************/
+
+export const GET_ALL_MEMBERS_BY_ADMIN = gql`
+  query GetAllMembersByAdmin($input: MembersInquiry!) {
+    getAllMembersByAdmin(input: $input) {
+      list {
+        _id
+        memberType
+        memberStatus
+        memberAuthType
+        memberPhone
+        memberNick
+        memberFullName
+        memberImage
+        memberAddress
+        memberDesc
+        memberProducts
+        memberArticles
+        memberFollowers
+        memberFollowings
+        memberPoints
+        memberLikes
+        memberViews
+        memberComments
+        memberRank
+        memberWarnings
+        memberBlocks
+        vendorNumber
+        deletedAt
+        createdAt
+        updatedAt
+        accessToken
+        meLiked {
+          memberId
+          likeRefId
+          myFavorite
+        }
+        meFollowed {
+          followingId
+          followerId
+          myFollowing
+        }
+      }
+      metaCounter {
+        total
+      }
+    }
+  }
+`;
+
+/**************************
+ *        PRODUCT        *
+ *************************/
+
+const GET_ALL_PRODUCTS_BY_ADMIN = gql`
+  query GetAllProductsByAdmin($input: AllProductsInquery!) {
+    getAllProductsByAdmin(input: $input) {
+      list {
+        _id
+        productCollection
+        productStatus
+        productName
+        productPrice
+        productOriginPrice
+        productViews
+        productLikes
+        productComments
+        productRank
+        productVolume
+        productLeftCount
+        productSoldCount
+        productOrigin
+        productDiscountRate
+        productImages
+        productDesc
+        productOwnerId
+        deletedAt
+        createdAt
+        updatedAt
+        productOwnerData {
+          _id
+          memberType
+          memberStatus
+          memberAuthType
+          memberPhone
+          memberNick
+          memberFullName
+          memberImage
+          memberAddress
+          memberDesc
+          memberProducts
+          memberArticles
+          memberFollowers
+          memberFollowings
+          memberPoints
+          memberLikes
+          memberViews
+          memberComments
+          memberRank
+          memberWarnings
+          memberBlocks
+          vendorNumber
+          deletedAt
+          createdAt
+          updatedAt
+          accessToken
+        }
+        meLiked {
+          memberId
+          likeRefId
+          myFavorite
+        }
+      }
+      metaCounter {
+        total
+      }
+    }
+  }
+`;
+
+/**************************
+ *      BOARD-ARTICLE     *
+ *************************/
+export const GET_ALL_BOARD_ARTICLES_BY_ADMIN = gql`
+  query GetAllBoardArticlesByAdmin($input: AllBoardArticlesInquiry!) {
+    getAllBoardArticlesByAdmin(input: $input) {
+      list {
+        _id
+        articleCategory
+        articleStatus
+        articleTitle
+        articleContent
+        articleImage
+        articleViews
+        articleLikes
+        articleComments
+        memberId
+        createdAt
+        updatedAt
+        memberData {
+          _id
+          memberType
+          memberStatus
+          memberAuthType
+          memberPhone
+          memberNick
+          memberFullName
+          memberImage
+          memberAddress
+          memberDesc
+          memberProducts
+          memberArticles
+          memberFollowers
+          memberFollowings
+          memberPoints
+          memberLikes
+          memberViews
+          memberComments
+          memberRank
+          memberWarnings
+          memberBlocks
+          deletedAt
+          createdAt
+          updatedAt
+          accessToken
+        }
+        meLiked {
+          memberId
+          likeRefId
+          myFavorite
+        }
+      }
+      metaCounter {
+        total
+      }
+    }
+  }
+`;
+
+/**************************
+ *         COMMENT        *
+ *************************/
+
+export const GET_COMMENTS = gql`
+  query GetComments($input: CommentsInquiry!) {
+    getComments(input: $input) {
+      list {
+        _id
+        commentStatus
+        commentGroup
+        commentContent
+        commentRefId
+        memberId
+        createdAt
+        updatedAt
+        memberData {
+          _id
+          memberType
+          memberStatus
+          memberAuthType
+          memberPhone
+          memberNick
+          memberFullName
+          memberImage
+          memberAddress
+          memberDesc
+          memberProducts
+          memberArticles
+          memberFollowers
+          memberFollowings
+          memberPoints
+          memberLikes
+          memberViews
+          memberComments
+          memberRank
+          memberWarnings
+          memberBlocks
+          vendorNumber
+          deletedAt
+          createdAt
+          updatedAt
+          accessToken
+        }
+      }
+      metaCounter {
+        total
+      }
+    }
+  }
+`;
--- pages/_admin/users/index.tsx
+++ pages/_admin/users/index.tsx
@@ -1,12 +1,59 @@
 import TopNavigation from "@/libs/components/admin/TopNavigation";
 import withLayoutAdmin from "@/libs/components/layout/AdminLayout";
 import { MembersInquiry } from "@/libs/types/member/member.input";
-import { Stack } from "@mui/material";
-import React, { useState } from "react";
+import { InputAdornment, OutlinedInput, Stack } from "@mui/material";
+import CancelRoundedIcon from "@mui/icons-material/CancelRounded";
+import React, { useCallback, useState } from "react";
+import { useQuery } from "@apollo/client";
+import { Member } from "@/libs/types/member/member";
+import { T } from "@/libs/types/common";
+import { GET_ALL_MEMBERS_BY_ADMIN } from "@/apollo/admin/query";
 
 const Users = ({ initialInquiry, ...props }: any) => {
   const [membersInquiry, setMembersInquiry] =
     useState<MembersInquiry>(initialInquiry);
+  const [members, setMembers] = useState<Member[]>([]);
+  const [membersTotal, setMembersTotal] = useState<number>(0);
+  const [searchText, setSearchText] = useState("");
+
+  //APOLLO REQUESTS
+  const {
+    loading: getAllMembersLoading,
+    data: getAllMembersData,
+    error: getAllMembersError,
+    refetch: getAllMembersRefetch,
+  } = useQuery(GET_ALL_MEMBERS_BY_ADMIN, {
+    fetchPolicy: "network-only",
+    variables: { input: membersInquiry },
+    notifyOnNetworkStatusChange: true,
+    onCompleted: (data: T) => {
+      setMembers(data?.getAllMembersByAdmin?.list);
+      setMembersTotal(data?.getAllMembersByAdmin?.metaCounter[0]?.total ?? 0);
+    },
+  });
+
+  //HANDLERS
+  const textHandler = useCallback((value: string) => {
+    try {
+      setSearchText(value);
+    } catch (err: any) {
+      console.log("textHandler: ", err.message);
+    }
+  }, []);
+
+  const searchTextHandler = () => {
+    try {
+      setMembersInquiry({
+        ...membersInquiry,
+        search: {
+          ...membersInquiry.search,
+          text: searchText,
+        },
+      });
+    } catch (err: any) {
+      console.log("searchTextHandler: ", err.message);
+    }
+  };
   return (
     <div className="users-page">
       <Stack className="users-page-intro">
@@ -14,6 +61,47 @@ const Users = ({ initialInquiry, ...props }: any) => {
         <span className="page-desc">View All Users</span>
       </Stack>
       <TopNavigation initialInquiry={membersInquiry} type="users" />
+      <Stack className="members-list-main-container">
+        <Stack className="search-container">
+          <OutlinedInput
+            value={searchText}
+            onChange={(e: any) => textHandler(e.target.value)}
+            sx={{ width: "100%" }}
+            className={"search"}
+            placeholder="Search user name"
+            onKeyDown={(event) => {
+              if (event.key == "Enter") searchTextHandler();
+            }}
+            endAdornment={
+              <>
+                {searchText && (
+                  <CancelRoundedIcon
+                    style={{ cursor: "pointer" }}
+                    onClick={async () => {
+                      setSearchText("");
+                      setMembersInquiry({
+                        ...membersInquiry,
+                        search: {
+                          ...membersInquiry.search,
+                          text: "",
+                        },
+                      });
+                      await getAllMembersRefetch({ input: membersInquiry });
+                    }}
+                  />
+                )}
+                <InputAdornment
+                  position="end"
+                  onClick={() => searchTextHandler()}
+                >
+                  <img src="/img/icons/search_icon.png" alt={"searchIcon"} />
+                </InputAdornment>
+              </>
+            }
+          />
+        </Stack>
+        <Stack className="members-list"></Stack>
+      </Stack>
     </div>
   );
 };
