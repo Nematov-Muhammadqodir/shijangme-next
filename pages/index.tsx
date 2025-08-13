30d35cec27fbe3be231664660b6ae4a87fa45924
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-08-13 21:57:49 +0900

    fix: modify logging directory for admin

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { userVar } from "@/apollo/store";
 import Advertisement from "@/libs/components/homePage/Advertisement";
 import Banner from "@/libs/components/homePage/Banner";
 import Blogs from "@/libs/components/homePage/Blogs";
@@ -6,11 +7,22 @@ import DiscounProductsList from "@/libs/components/homePage/DiscounProductsList"
 import NewProductsList from "@/libs/components/homePage/NewProductsList";
 import TrendProductsList from "@/libs/components/homePage/TrendProductsList";
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
+import { useReactiveVar } from "@apollo/client";
 import { Box, Stack } from "@mui/material";
 import { NextPage } from "next";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
 
 const Home: NextPage = () => {
+  const user = useReactiveVar(userVar);
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log("userTypeHome", user?.memberType);
+    if (user.memberType === "ADMIN") {
+      router.push("/_admin/users");
+    }
+  }, [user]);
   return (
     <Stack className="home-page" sx={{ height: "600px", marginTop: "200px" }}>
       <Banner />
